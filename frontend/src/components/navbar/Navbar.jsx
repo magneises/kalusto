078807d211715c48b6c6d38856392ac5821610a5
Preston G <prestongilmore@gmail.com>
2025-03-15 15:21:23 -0600

    created new folder components and two new navbar files

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className={styles.NavbarContainer}>
+        <h2>This is the Navbar component</h2>
+    
+    </div>
+  )
+}
+
+export default Navbar
