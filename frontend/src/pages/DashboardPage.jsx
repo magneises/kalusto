0fe5ceee8375279403838f09c13f204cfac04754
Preston G <prestongilmore@gmail.com>
2025-03-22 23:58:36 -0600

    updating search and dashboard

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -109,9 +109,43 @@ const DashboardPage = () => {
         }
     };
 
+    const SearchPage = () => {
+        const [symbol, setSymbol] = useState("");
+        const navigate = useNavigate();
+    
+        const handleSearch = () => {
+            if (symbol.length > 0) {
+                navigate(`/dashboard?symbol=${symbol.toUpperCase()}`);
+            }
+        };
+    };
+
+
+
+    // <h1>Stock Market Search</h1>
+    // <input
+    //     type="text"
+    //     placeholder="Enter stock symbol (e.g., AAPL)"
+    //     value={symbol}
+    //     onChange={(e) => setSymbol(e.target.value)}
+    // />
+    // <button onClick={handleSearch}>Search</button>
+
+
     return (
         <div style={{ maxWidth: "800px", margin: "0 auto" }}>
             <h1>{symbol} Dashboard</h1>
+
+            <h1>input</h1>
+           <input>
+           
+           </input>
+
+
+
+            
+
+            
             
             {/* View Selector Dropdown */}
             <select value={view} onChange={(e) => setView(e.target.value)}>
