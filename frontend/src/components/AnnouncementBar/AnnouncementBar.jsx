ea7567bff23ab0953569e6e865670a7dc29959ca
Preston G <prestongilmore@gmail.com>
2025-04-09 22:37:22 -0600

    prepare module.css announcementbar

--- frontend/src/components/AnnouncementBar/AnnouncementBar.jsx
+++ frontend/src/components/AnnouncementBar/AnnouncementBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AnnouncementBar() {
   return (
-    <div>
+    <div className="styles.announcementContainer">
         Please note that initial loading times may vary, if data doesn't load after a minute, please refresh your browser. 
     </div>
   )
